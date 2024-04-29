24bde4c66f6bf9ca4ee60138b25403d43081697a
LUKITA1416171822 <132844508+LUKITA1416171822@users.noreply.github.com>
2024-04-29 17:18:17 +0300

    Trying different views for profile

--- src/pages/dashboard-and-profile/ProfilePage.jsx
+++ src/pages/dashboard-and-profile/ProfilePage.jsx
@@ -4,6 +4,7 @@ import {useParams} from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
 import {ProfileCard} from '../../components/ProfileCard.jsx';
 import individualUserSlice from "../../redux/IndividualUserSlice.js";
+import {StyledInput} from "../../components/StyledInput.jsx";
 
 export default function ProfilePage(){
     const {profileType , userID} = useParams();
@@ -27,16 +28,30 @@ export default function ProfilePage(){
                 User != null && <div className="flex justify-center items-center min-h-screen">
 
                     <div
-                        className=" w-1/2 bg-slate-50  border-Tropical-Lagoon hover:shadow-Tropical-Lagoon border-2 rounded-3xl p-7
-                           flex-col gap-8 justify-between hover:bg-slate-100
+                        className=" w-1/2   bg-Midnight-Pine border-Tropical-Lagoon hover:shadow-Tropical-Lagoon border-2 rounded-3xl p-7
+                           flex-col gap-8 justify-between
                             transition-shadow ">
-                        <Avatar>{User.firstName.charAt(0) + "" + User.lastName.charAt(0)}</Avatar>
+                        <Avatar sx={{width: 100, height: 100}}>
+                            <div className="text-6xl ">{User.firstName.charAt(0) + "" + User.lastName.charAt(0)}</div>
+                        </Avatar>
+                        <div className={' flex flex-row justify-between m-2'}>
+                            <div className="text-2xl">FirstName: {User.firstName}</div>
+                            <div className="text-2xl">LastName: {User.lastName}</div>
+                        </div>
 
-                        <div className={'flex-row'}><h1>Number: {User.contactNumber}</h1>
-                            <h1>Password: {User.password}</h1></div>
-                        <h1>Address: {User.address}</h1>
-                        <h1>Area: {User.area}</h1>
-                        <h1>Governorate: {User.governorate}</h1>
+                        <div className={' flex flex-row justify-between m-2 '}>
+                            <StyledInput  type="text" text="Email" id="email" value={User.email} />
+                            <div className="text-2xl">Password: {User.password}</div>
+                        </div>
+                        <div className={' flex flex-row justify-between m-2'}>
+                            <div className="text-2xl ">Number: {User.contactNumber}</div>
+                            <div className="text-2xl ">Governorate: {User.governorate}</div>
+                        </div>
+                        <div className={' flex flex-row justify-between m-2'}>
+                            <div className="text-2xl">Area: {User.area}</div>
+                            <div className="text-2xl">Address: {User.address}</div>
+                        </div>
+
                     </div>
                 </div>
             }
