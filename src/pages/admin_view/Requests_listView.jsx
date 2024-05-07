7eafb7e569f3b74e8b78cb0b1cd42e38bc1d04d2
LUKITA1416171822 <132844508+LUKITA1416171822@users.noreply.github.com>
2024-05-07 10:43:02 +0300

    kunt 3aoz a pull M_2 3nd yehia

--- src/pages/admin_view/Requests_listView.jsx
+++ src/pages/admin_view/Requests_listView.jsx
@@ -9,7 +9,7 @@ export default function Requests_listView() {
 
 
     return (<>
-            <MainHeader/>
+            <MainHeaders/>
         <div
             className="flex flex-col justify-around items-center w-3/4  space-x-4 px-2 py-2 rounded-md shadow-md bg-red-500 dark:bg-red-600">
             <h1 className="text-2xl font-semibold text-white dark:text-gray-200">Requests</h1>
