7d41045a502856526c406d3e5eda86268eb4d975
welo5 <abdokhalil822005@gmail.com>
2024-05-12 16:27:33 +0300

    adding tab

--- src/pages/viewPages/ViewDonationHistory.jsx
+++ src/pages/viewPages/ViewDonationHistory.jsx
@@ -6,7 +6,20 @@ export default function ViewDonationHistory(){
         <div className={"h-screen w-screen bg-teal-50 flex flex-col items-center justify-start overflow-hidden"}>
             <MainHeader />
             <div className={"flex flex-col flex-grow items-center py-4"}>
-
+                <Tabs aria-label="Dynamic tabs" items={tabs} variant={"bordered"}
+                classNames={{
+                    tabList: "gap-6 w-full p-0 bg-slate-200",
+                    cursor: "w-full bg-Deep-Sea-Emerald",
+                    tab: "max-w-fit px-2 h-12",
+                    tabContent: "text-slate-950 group-data-[selected=true]:text-slate-50"
+                }}
+                >
+                    {(item) => (
+                        <Tab key={item.id} title={item.title}>
+                            <h1>{item.title}</h1>
+                        </Tab>
+                    )}
+                </Tabs>
             </div>
         </div>
     )
